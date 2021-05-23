766c678c652aeb0063ac44731d5f8e8bf997eded
geppy00 <76499253+geppy00@users.noreply.github.com>
2021-05-23 13:10:51 +0200

    completate le animazioni

--- Omnifood Project/resources/js/script.js
+++ Omnifood Project/resources/js/script.js
@@ -60,6 +60,68 @@ $(document).ready(function() {
             });
           }
         }
-      });
+    });
+    
+    
+    /*Animation on scroll*/
+    $('.js--wp-1').waypoint(
+        (direction) => {
+              $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+        },
+        {
+            offset: '50%',
+        }
+    );
+    
+   
+    $('.js--wp-2').waypoint(
+        (direction) => {
+              $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+        },
+        {
+            offset: '50%',
+        }
+    );
+    
+    
+    $('.js--wp-3').waypoint(
+        (direction) => {
+              $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+        },
+        {
+            offset: '50%',
+        }
+    );
+    
+    
+    $('.js--wp-4').waypoint(
+        (direction) => {
+              $('.js--wp-4').addClass('animate__animated animate__pulse');
+        },
+        {
+            offset: '50%',
+        }
+    );
+    
+    
+    /*Mobile nav*/
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        
+        if(icon.hasClass('ion-navicon-round')) {
+            
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+            
+    });
+    
+
     
 });
